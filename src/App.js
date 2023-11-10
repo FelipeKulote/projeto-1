5febb1323d250ff4dfadd8d53357f625fa806ccb
Felipe Augusto <lipe_augustobq@hotmail.com>
2023-11-09 23:31:37 -0300

    Learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Titulo 1",
+        body: "Corpo 1",
+      },
+      {
+        id: 2,
+        title: "Titulo 2",
+        body: "Corpo 2",
+      },
+      {
+        id: 3,
+        title: "Titulo 3",
+        body: "Corpo 3",
+      },
+    ],
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { posts.map(post => (
+      <div key={post.id}>
+        <h1>{ post.title }</h1>
+        <p>{post.body}</p>
+      </div>
+      )) }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
